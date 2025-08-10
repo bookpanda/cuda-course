ae17fd4e0d95e86011bec5922300e80a3860a807
Idhibhat Pankam <marginpankam@gmail.com>
2025-08-10 14:19:44 +0700

    custom types

--- 03_C_and_C++_Review/02 Custom Types/01.c
+++ 03_C_and_C++_Review/02 Custom Types/01.c
@@ -1,4 +1,4 @@
-// size_t = size type for memory allocation
+// size_t = size type for memory allocation (same as unsigned long)
 // size_t is an unsigned integer data type used to represent the size of objects in bytes. 
 // It is guaranteed to be big enough to contain the size of the biggest object the host system can handle.
 
--- 03_C_and_C++_Review/02 Custom Types/02.cpp
+++ 03_C_and_C++_Review/02 Custom Types/02.cpp
@@ -5,12 +5,13 @@
 using namespace std;
 
 typedef struct {
-    float x;
-    float y;
+  float x;
+  float y;
 } Point;
 
 int main() {
-    Point p = {1.1, 2.5};
-    // printf("size of Point: %zu\n", sizeof(Point));  // Output: 8 bytes = 4 bytes (float x) + 4 bytes (float y)
-    
+  Point p = {1.1, 2.5};
+  printf(
+      "size of Point: %zu\n",
+      sizeof(Point)); // Output: 8 bytes = 4 bytes (float x) + 4 bytes (float y)
 }
